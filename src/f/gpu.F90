4801ccbbd76ea7414b282d89bbfa21ab84f22fbb
Thomas Body <thomas.body@ipp.mpg.de>
2021-07-25 16:36:02 +0200

    Use MPI_MAX

--- src/f/gpu.F90
+++ src/f/gpu.F90
@@ -203,34 +203,8 @@ PROGRAM main
         ! //////////////////////////////////////////////////////////
         ! // -- SUBTASK 4: FIND MAX TEMPERATURE CHANGE OVERALL -- //
         ! //////////////////////////////////////////////////////////
-        IF (my_rank .NE. MASTER_PROCESS_RANK) THEN
-            ! Send my temperature delta to the master MPI process
-            CALL MPI_Ssend(my_temperature_change, 1, MPI_DOUBLE_PRECISION, MASTER_PROCESS_RANK, 0, MPI_COMM_WORLD, ierr)
-            
-            ! Receive the total delta calculated by the MPI process based on all MPI processes delta
-            CALL MPI_Recv(global_temperature_change, 1, MPI_DOUBLE_PRECISION, MASTER_PROCESS_RANK, MPI_ANY_TAG, MPI_COMM_WORLD, &
-                          MPI_STATUS_IGNORE, ierr)
-        ELSE
-            ! Initialise the temperature change to mine
-            global_temperature_change = my_temperature_change
 
-            ! Pick highest temperature change observed
-            DO j = 0, comm_size-1
-                IF (j .NE. my_rank) THEN
-                    CALL MPI_Recv(subtotal, 1, MPI_DOUBLE_PRECISION, j, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr)
-                    IF (subtotal .GT. global_temperature_change) THEN
-                        global_temperature_change = subtotal
-                    END IF
-                END IF
-            END DO
-
-            ! Send delta back to all MPI processes
-            DO j = 0, comm_size-1
-                IF (j .NE. my_rank) THEN
-                    CALL MPI_Ssend(global_temperature_change, 1, MPI_DOUBLE_PRECISION, j, 0, MPI_COMM_WORLD, ierr)
-                END IF
-            END DO
-        END IF
+        call MPI_reduce(my_temperature_change, global_temperature_change, 1, MPI_DOUBLE_PRECISION, MPI_MAX, MASTER_PROCESS_RANK, MPI_COMM_WORLD, ierr)
 
         ! //////////////////////////////////////////////////
         ! // -- SUBTASK 5: UPDATE LAST ITERATION ARRAY -- //
