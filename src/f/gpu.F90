9092cc6e7ed7f1209b4b027bf0c7106bd559e41d
Thomas Body <thomas.body@ipp.mpg.de>
2021-07-28 17:22:41 +0200

    Shorthand

--- src/f/gpu.F90
+++ src/f/gpu.F90
@@ -10,6 +10,8 @@ PROGRAM main
     use openacc
 
     IMPLICIT NONE
+
+    integer, parameter :: COLS = COLUMNS, ROWS_MPI = ROWS_PER_MPI_PROCESS, COLS_MPI = COLUMNS_PER_MPI_PROCESS
    
     !> Used to get the error code returned by MPI routines
     INTEGER :: ierr
@@ -32,11 +34,11 @@ PROGRAM main
     !> Rank of my bottom neighbour if any
     INTEGER :: S_rank
     !> Array that will contain my part chunk. It will include the 2 ghost rows (1 left, 1 right)
-    REAL(8), DIMENSION(1:ROWS_PER_MPI_PROCESS,0:COLUMNS_PER_MPI_PROCESS+1) :: temperatures
+    REAL(8), DIMENSION(1:ROWS_MPI,0:COLS_MPI+1) :: temperatures
     !> Temperatures from the previous iteration, same dimensions as the array above.
-    REAL(8), DIMENSION(1:ROWS_PER_MPI_PROCESS,0:COLUMNS_PER_MPI_PROCESS+1) :: temperatures_last
+    REAL(8), DIMENSION(1:ROWS_MPI,0:COLS_MPI+1) :: temperatures_last
     !> On master process only: contains all temperatures read from input file.
-    REAL(8), DIMENSION(0:ROWS-1,0:COLUMNS-1) :: all_temperatures
+    REAL(8), DIMENSION(0:ROWS-1,0:COLS-1) :: all_temperatures
     !> Will contain the entire time elapsed in the timed portion of the code
     REAL(8) :: total_time_so_far = 0.0
     !> Contains the timestamp as measured at the beginning of the timed portion of the code
@@ -58,7 +60,7 @@ PROGRAM main
     !> Used to store temperatures changes from other MPI processes
     REAL(8) :: subtotal
     !> The last snapshot made
-    REAL(8), DIMENSION(1:ROWS,1:COLUMNS) :: snapshot
+    REAL(8), DIMENSION(1:ROWS,1:COLS) :: snapshot
     real(8), parameter :: one_third = 1.0_8 / 3.0_8
     integer :: ndev, idev
     integer :: reduce_req, gather_req, bcast_req
@@ -70,8 +72,8 @@ PROGRAM main
     !> Dimensions (x and y)
     integer, parameter :: XDIM = 0, YDIM = 1
     !> Dimension sizes
-    integer, parameter :: NX = COLUMNS/COLUMNS_PER_MPI_PROCESS
-    integer, parameter :: NY = ROWS/ROWS_PER_MPI_PROCESS
+    integer, parameter :: NX = COLS/COLS_MPI
+    integer, parameter :: NY = ROWS/ROWS_MPI
     integer, dimension(0:1) :: dims, coords
     logical, parameter :: check_snapshot = .false.
     CALL MPI_Init(ierr)
@@ -83,7 +85,7 @@ PROGRAM main
     CALL MPI_Comm_size(MPI_COMM_WORLD, comm_size, ierr)
     
     if (.not.(dims(0) * dims(1) == comm_size)) then
-        print *, "Error: could not decompose MPI with given ROWS_PER_MPI_PROCESS and COLUMNS_PER_MPI_PROCESS", NX, NY, comm_size
+        print *, "Error: could not decompose MPI with given ROWS_MPI and COLS_MPI", NX, NY, comm_size
     endif
 
     CALL MPI_cart_create(MPI_COMM_WORLD, 2, dims, &
@@ -122,20 +124,20 @@ PROGRAM main
     ! ////////////////////////////////////////////////////////
     start_time = MPI_Wtime()
 
-    CALL MPI_scatter(all_temperatures, ROWS_PER_MPI_PROCESS * COLUMNS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, &
-                     temperatures_last(1,1), ROWS_PER_MPI_PROCESS * COLUMNS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, MASTER_PROCESS_RANK, &
+    CALL MPI_scatter(all_temperatures, ROWS_MPI * COLS_MPI, MPI_DOUBLE_PRECISION, &
+                     temperatures_last(1,1), ROWS_MPI * COLS_MPI, MPI_DOUBLE_PRECISION, MASTER_PROCESS_RANK, &
                      cart_comm, ierr)
     
     ! Copy the temperatures into the current iteration temperature as well
     !$acc data create(temperatures) copyin(temperatures_last)
     !$acc kernels
-    DO j = 1, COLUMNS_PER_MPI_PROCESS
-        DO i = 1, ROWS_PER_MPI_PROCESS
+    DO j = 1, COLS_MPI
+        DO i = 1, ROWS_MPI
             temperatures(i,j) = temperatures_last(i,j)
         ENDDO
     ENDDO
     !$acc end kernels
-    !$acc update host(temperatures(:,1), temperatures(:,COLUMNS_PER_MPI_PROCESS)) async(1)
+    !$acc update host(temperatures(:,1), temperatures(:,COLS_MPI)) async(1)
 
     DO WHILE (total_time_so_far .LT. MAX_TIME)
 
@@ -151,95 +153,88 @@ PROGRAM main
             call MPI_ireduce(my_temperature_change, global_temperature_change, 1, MPI_DOUBLE_PRECISION, MPI_MAX, &
                              MASTER_PROCESS_RANK, cart_comm, reduce_req, ierr)
             
-            !$acc update host(temperatures(1:ROWS_PER_MPI_PROCESS, 1:COLUMNS_PER_MPI_PROCESS))
+            !$acc update host(temperatures(1:ROWS_MPI, 1:COLS_MPI))
             ! Verified that the sum of the gather is equal to the sum of the individual sends and recieves 
-            call MPI_igather(temperatures(1,1), ROWS_PER_MPI_PROCESS * COLUMNS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, &
-                    snapshot, ROWS_PER_MPI_PROCESS * COLUMNS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, &
+            call MPI_igather(temperatures(1,1), ROWS_MPI * COLS_MPI, MPI_DOUBLE_PRECISION, &
+                    snapshot, ROWS_MPI * COLS_MPI, MPI_DOUBLE_PRECISION, &
                     MASTER_PROCESS_RANK, cart_comm, gather_req, ierr)
 
         END IF
         
         !$acc wait(1)
-        ! Send data to up neighbour for its ghost cells. If my W_rank is MPI_PROC_NULL, this MPI_Ssend will do nothing.
-        CALL MPI_Isend(temperatures(:,1), ROWS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, W_rank, &
+        CALL MPI_Isend(temperatures(:,1), ROWS_MPI, MPI_DOUBLE_PRECISION, W_rank, &
                        101, cart_comm, W_send_req, ierr)
-
-        ! Receive data from down neighbour to fill our ghost cells. If my E_rank is MPI_PROC_NULL, this MPI_Recv will do nothing.
-        CALL MPI_IRecv(temperatures_last(:,COLUMNS_PER_MPI_PROCESS+1), ROWS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, E_rank, &
+        CALL MPI_IRecv(temperatures_last(:,COLS_MPI+1), ROWS_MPI, MPI_DOUBLE_PRECISION, E_rank, &
                        101, cart_comm, E_recv_req, ierr)
-
-        ! Send data to down neighbour for its ghost cells. If my E_rank is MPI_PROC_NULL, this MPI_Ssend will do nothing.
-        CALL MPI_Isend(temperatures(:, COLUMNS_PER_MPI_PROCESS), ROWS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, E_rank, &
+        CALL MPI_Isend(temperatures(:, COLS_MPI), ROWS_MPI, MPI_DOUBLE_PRECISION, E_rank, &
                        102, cart_comm, E_send_req, ierr)
-
-        ! Receive data from up neighbour to fill our ghost cells. If my W_rank is MPI_PROC_NULL, this MPI_Recv will do nothing.
-        CALL MPI_IRecv(temperatures_last(:,0), ROWS_PER_MPI_PROCESS, MPI_DOUBLE_PRECISION, W_rank, &
+        CALL MPI_IRecv(temperatures_last(:,0), ROWS_MPI, MPI_DOUBLE_PRECISION, W_rank, &
                        102, cart_comm, W_recv_req, ierr)
-        
+
         my_temperature_change = 0.0
         !$acc kernels async(2)
-        DO j = 2, COLUMNS_PER_MPI_PROCESS - 1
+        DO j = 2, COLS_MPI - 1
             ! Process all cells between the first and last columns excluded, which each has both left and right neighbours
-            DO i = 2, ROWS_PER_MPI_PROCESS - 1
+            DO i = 2, ROWS_MPI - 1
                     temperatures(i,j) = 0.25 * (temperatures_last(i-1,j  ) + &
                                                 temperatures_last(i+1,j  ) + &
                                                 temperatures_last(i  ,j-1) + &
                                                 temperatures_last(i  ,j+1))
             END DO
         END DO
-        DO j = 2, COLUMNS_PER_MPI_PROCESS - 1
+        DO j = 2, COLS_MPI - 1
             ! Process the cell at the first row, which has no up neighbour
             temperatures(1,j) = (temperatures_last(1,j-1) + &
                                  temperatures_last(1,j+1) + &
                                  temperatures_last(2,j  )) * one_third
             ! Process the cell at the bottom row, which has no down neighbour
-            temperatures(ROWS_PER_MPI_PROCESS,j) = (temperatures_last(ROWS_PER_MPI_PROCESS, j - 1) + &
-                                                    temperatures_last(ROWS_PER_MPI_PROCESS, j + 1) + &
-                                                    temperatures_last(ROWS_PER_MPI_PROCESS  -1, j)) * one_third
+            temperatures(ROWS_MPI,j) = (temperatures_last(ROWS_MPI, j - 1) + &
+                                                    temperatures_last(ROWS_MPI, j + 1) + &
+                                                    temperatures_last(ROWS_MPI  -1, j)) * one_third
         ENDDO
         !$acc end kernels
 
         call MPI_WAITALL(4, (/ W_send_req, E_send_req, W_recv_req, E_recv_req /), MPI_STATUSES_IGNORE, ierr)
-        !$acc update device(temperatures_last(:,0), temperatures_last(:,COLUMNS_PER_MPI_PROCESS+1))
+        !$acc update device(temperatures_last(:,0), temperatures_last(:,COLS_MPI+1))
         !$acc kernels async(3)
-        DO i = 2, ROWS_PER_MPI_PROCESS - 1
+        DO i = 2, ROWS_MPI - 1
             temperatures(i,1) = 0.25 * (temperatures_last(i-1,1) + temperatures_last(i+1,1) &
                                       + temperatures_last(i,1-1) + temperatures_last(i,1+1))
-            temperatures(i,COLUMNS_PER_MPI_PROCESS) = 0.25 * (temperatures_last(i-1,COLUMNS_PER_MPI_PROCESS) &
-                                                            + temperatures_last(i+1,COLUMNS_PER_MPI_PROCESS) &
-                                                            + temperatures_last(i,COLUMNS_PER_MPI_PROCESS-1) &
-                                                            + temperatures_last(i,COLUMNS_PER_MPI_PROCESS+1))
+            temperatures(i,COLS_MPI) = 0.25 * (temperatures_last(i-1,COLS_MPI) &
+                                                            + temperatures_last(i+1,COLS_MPI) &
+                                                            + temperatures_last(i,COLS_MPI-1) &
+                                                            + temperatures_last(i,COLS_MPI+1))
         END DO
         
         ! Fill in corners
         temperatures(1,1) = (temperatures_last(1,0) + temperatures_last(1,2) &
                            + temperatures_last(2,1)) * one_third
         
-        temperatures(ROWS_PER_MPI_PROCESS,1) = (temperatures_last(ROWS_PER_MPI_PROCESS, 1 - 1) &
-                                              + temperatures_last(ROWS_PER_MPI_PROCESS, 1 + 1) &
-                                              + temperatures_last(ROWS_PER_MPI_PROCESS-1, 1)) * one_third
+        temperatures(ROWS_MPI,1) = (temperatures_last(ROWS_MPI, 1 - 1) &
+                                              + temperatures_last(ROWS_MPI, 1 + 1) &
+                                              + temperatures_last(ROWS_MPI-1, 1)) * one_third
 
-        temperatures(1,COLUMNS_PER_MPI_PROCESS) = (temperatures_last(1,COLUMNS_PER_MPI_PROCESS-1) &
-                                                 + temperatures_last(1,COLUMNS_PER_MPI_PROCESS+1) &
-                                                 + temperatures_last(2,COLUMNS_PER_MPI_PROCESS)) * one_third 
+        temperatures(1,COLS_MPI) = (temperatures_last(1,COLS_MPI-1) &
+                                                 + temperatures_last(1,COLS_MPI+1) &
+                                                 + temperatures_last(2,COLS_MPI)) * one_third 
 
-        temperatures(ROWS_PER_MPI_PROCESS,COLUMNS_PER_MPI_PROCESS) = &
-                 (temperatures_last(ROWS_PER_MPI_PROCESS, COLUMNS_PER_MPI_PROCESS - 1) &
-                + temperatures_last(ROWS_PER_MPI_PROCESS, COLUMNS_PER_MPI_PROCESS + 1) &
-                + temperatures_last(ROWS_PER_MPI_PROCESS-1, COLUMNS_PER_MPI_PROCESS)) * one_third
+        temperatures(ROWS_MPI,COLS_MPI) = &
+                 (temperatures_last(ROWS_MPI, COLS_MPI - 1) &
+                + temperatures_last(ROWS_MPI, COLS_MPI + 1) &
+                + temperatures_last(ROWS_MPI-1, COLS_MPI)) * one_third
         
         !$acc end kernels 
         
         !$acc kernels wait(2, 3)
         temperatures = merge(temperatures, temperatures_last, temperatures_last /= MAX_TEMPERATURE)
-        DO j = 1, COLUMNS_PER_MPI_PROCESS
-            DO i = 1, ROWS_PER_MPI_PROCESS
+        DO j = 1, COLS_MPI
+            DO i = 1, ROWS_MPI
                 my_temperature_change = max(abs(temperatures(i,j) - temperatures_last(i,j)), my_temperature_change)
                 temperatures_last(i,j) = temperatures(i,j)
             END DO
         END DO
         !$acc end kernels
-        !$acc update host(temperatures(:,1), temperatures(:,COLUMNS_PER_MPI_PROCESS)) async(1)
+        !$acc update host(temperatures(:,1), temperatures(:,COLS_MPI)) async(1)
 
         IF (MOD(iteration_count, SNAPSHOT_INTERVAL) .EQ. 0) THEN
             CALL MPI_WAITALL(3, (/ reduce_req, bcast_req, gather_req  /), MPI_STATUSES_IGNORE, ierr)
