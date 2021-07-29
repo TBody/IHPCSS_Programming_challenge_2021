91cb3922e910f89172283a5923b698f5430de300
Thomas Body <thomas.body@ipp.mpg.de>
2021-07-29 09:59:40 +0200

    Make a buffer for temp send

--- src/f/gpu.F90
+++ src/f/gpu.F90
@@ -67,6 +67,10 @@ PROGRAM main
     real(8), parameter :: one_third = 1.0_8 / 3.0_8
     integer :: reduce_req, gather_req, bcast_req
 
+    REAL(8), DIMENSION(0:ROWS_MPI-1,0:COLS_MPI-1) :: temp_buffer
+    REAL(8) :: max_temp_change
+
+
     CALL MPI_Init(ierr)
     ! /////////////////////////////////////////////////////
     ! ! -- PREPARATION 1: COLLECT USEFUL INFORMATION -- //
@@ -190,7 +194,8 @@ PROGRAM main
         ! //////////////////////////////////////////////////////////
         ! // -- SUBTASK 4: FIND MAX TEMPERATURE CHANGE OVERALL -- //
         ! //////////////////////////////////////////////////////////
-        call MPI_ireduce(my_temperature_change, global_temperature_change, 1, MPI_DOUBLE_PRECISION, MPI_MAX, &
+        max_temp_change = my_temperature_change
+        call MPI_ireduce(max_temp_change, global_temperature_change, 1, MPI_DOUBLE_PRECISION, MPI_MAX, &
                              MASTER_PROCESS_RANK, cart_comm, reduce_req, ierr)
         CALL MPI_WAIT(reduce_req, MPI_STATUS_IGNORE, ierr)
 
@@ -207,8 +212,10 @@ PROGRAM main
         ! // -- SUBTASK 6: GET SNAPSHOT -- //
         ! ///////////////////////////////////
         IF (MOD(iteration_count, SNAPSHOT_INTERVAL) .EQ. 0) THEN
+            
+            temp_buffer = temperatures(0:ROWS_MPI, 1:COLS_MPI+1)
 
-            call MPI_igather(temperatures(0,1), ROWS_MPI * COLS_MPI, MPI_DOUBLE_PRECISION, &
+            call MPI_igather(temp_buffer, ROWS_MPI * COLS_MPI, MPI_DOUBLE_PRECISION, &
                     snapshot, ROWS_MPI * COLS_MPI, MPI_DOUBLE_PRECISION, &
                     MASTER_PROCESS_RANK, cart_comm, gather_req, ierr)
 
